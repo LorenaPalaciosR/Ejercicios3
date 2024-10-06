227bb168dd146d262c609e962a3287d436bfef53
JosuMoi270706 <moiseshdz7127@gmail.com>
2024-10-06 15:59:14 -0600

    Ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -4,3 +4,22 @@ ignorando espacios y mayúsculas. Por ejemplo, "Anita lava la tina" es un palín
 Escribe una función que reciba una cadena de caracteres y determine si es un palíndromo. 
 La función debe ser capaz de ignorar los espacios y no diferenciar entre letras mayúsculas y minúsculas. 
 Debes recorrer la cadena y comparar sus caracteres de manera inversa sin usar el método de slicing ([::-1])."""
+def es_palindromo(cadena):
+    #Limpiar la cadena: quitar espacios y convertir a minúsculas
+    cadena_limpia ="".join(cadena.split()).lower()
+    
+    #Longitud de la cadena limpia
+    longitud = len(cadena_limpia)
+    
+    #Comparar caracteres desde el inicio y el final
+    for i in range(longitud // 2):
+        if cadena_limpia[i] != cadena_limpia[longitud - 1 - i]:
+           return f´´"{cadena}" no es un palíndromo.´
+    
+    return f´"(cadena)" es un palíndromo.´
+
+#Ejemplo de uso
+frase = "Anita lava la tinta"
+print(es_palindromo(frase)) #Salida: "Anita lava la tinta" es un palíndromo.
+  
+              
