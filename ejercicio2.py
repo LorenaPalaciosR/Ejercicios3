2ca390daef4a5c0554204cc3707a973556dc563c
nisepesa <nisepesa1011@gmail.com>
2024-10-09 20:08:15 -0500

    realizar pc3

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,3 +3,16 @@ El objetivo de este ejercicio es escribir una función que tome una cadena de ca
 pero sin hacer uso del operador de slicing ([::-1]). La función debe recorrer cada carácter de la cadena de entrada 
 y construir una nueva cadena con los caracteres en orden inverso. Por ejemplo, si la función recibe "python", 
 debe retornar "nohtyp". Asegúrate de no usar atajos como métodos incorporados que realicen la inversión por ti."""
+
+def invertir_cadena(cadena):
+    cadena_invertida = ""
+    for caracter in cadena:
+        cadena_invertida = caracter + cadena_invertida
+    return cadena_invertida
+
+palabra_usuario = input("Ingresa una palabra: ")
+
+resultado = invertir_cadena(palabra_usuario)
+
+print(f"La palabra invertida es: {resultado}")
+ 
